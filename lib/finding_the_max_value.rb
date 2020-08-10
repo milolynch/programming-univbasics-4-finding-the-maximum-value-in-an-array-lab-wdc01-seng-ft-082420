0f0c4256b827fa36618366f555a91e3db2e368fb
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-10 17:37:02 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,7 +2,7 @@ def find_max_value(array)
   count = 0
   mav_v = 0
   while count < array.length do
-    if array[count] > max _v
+    if array[count] > max_v
       max_v = array[count]
     end
     count += 1
